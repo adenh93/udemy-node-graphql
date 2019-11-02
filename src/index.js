978074be9c0f53f84e7c5c9289c1335dae041ede
Aden Herold <aden.herold1@gmail.com>
2019-11-02 16:05:25 +0930

    Added custom types User and Post and added to the query type as 'me' and 'post'. Added dummy resolvers for me and post

--- src/index.js
+++ src/index.js
@@ -3,27 +3,43 @@ import { GraphQLServer } from "graphql-yoga";
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        hello: String!
+        me: User!
+        post: Post!
+    }
+
+    type User {
+        id: ID!
         name: String!
-        location: String!
-        bio: String!
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
     }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
-    hello() {
-      return "Hello from GraphQL!";
+    me() {
+      return {
+        id: "abc123",
+        name: "Some User",
+        email: "mail@mail.com",
+        age: 26
+      };
     },
-    name() {
-      return "Aden Herold";
-    },
-    location() {
-      return "Darwin, NT";
-    },
-    bio() {
-      return "I am a software developer living and working in the Northern Territory.";
+    post() {
+      return {
+        id: "abc123",
+        title: "Some Post Title",
+        body: "This is the post's body",
+        published: true
+      };
     }
   }
 };
