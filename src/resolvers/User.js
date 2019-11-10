9c311df8c0b61b3acab64e7194b7a195e638a12d
Aden Herold <aden.herold1@gmail.com>
2019-11-10 13:44:25 +0930

    Modified User resolver to only return an email when queried if the email belongs to the currently authenticated user

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,1 +1,8 @@
-export const User = {};
+import getUserId from "../utils/getUserId";
+
+export const User = {
+  email({ id, email }, args, { request }, info) {
+    const userId = getUserId(request, false);
+    return userId === id ? email : null;
+  }
+};
