d9e8ccd10c8bcff9918d3d6a4499f0fc972405df
Aden Herold <aden.herold1@gmail.com>
2019-11-13 20:06:59 +0930

    Moved code to create new instance of ApolloBoost client into utils/getClient.js, and imported into test suites

--- backend/tests/user.test.js
+++ backend/tests/user.test.js
@@ -1,11 +1,10 @@
 import "cross-fetch/polyfill";
-import ApolloBoost, { gql } from "apollo-boost";
+import { gql } from "apollo-boost";
 import prisma from "../src/prisma";
+import getClient from "./utils/getClient";
 import seedDatabase from "./utils/seedDatabase";
 
-const client = new ApolloBoost({
-  uri: "http://localhost:4000"
-});
+const client = getClient();
 
 beforeEach(seedDatabase);
 
