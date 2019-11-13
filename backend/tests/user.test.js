3109508d8d4dc311895a2d10d871cfc39f390f67
Aden Herold <aden.herold1@gmail.com>
2019-11-13 19:12:51 +0930

    Added beforeEach jest global to user.test.js to wipe all records from the database before each test runs. Added user and posts data to populate the database before each test.

--- backend/tests/user.test.js
+++ backend/tests/user.test.js
@@ -1,9 +1,48 @@
 import "cross-fetch/polyfill";
 import ApolloBoost, { gql } from "apollo-boost";
+import hashPassword from "../src/utils/hashPassword";
 import prisma from "../src/prisma";
 
 const client = new ApolloBoost({
   uri: "http://localhost:4000"
+});
+
+beforeEach(async () => {
+  await prisma.mutation.deleteManyUsers();
+
+  const { id } = await prisma.mutation.createUser({
+    data: {
+      name: "Test User",
+      email: "test123@test.com",
+      password: await hashPassword("testPassword123")
+    }
+  });
+
+  await prisma.mutation.createPost({
+    data: {
+      title: "Test Post 1",
+      body: "Test Body",
+      published: true,
+      author: {
+        connect: {
+          id
+        }
+      }
+    }
+  });
+
+  await prisma.mutation.createPost({
+    data: {
+      title: "Test Post 2",
+      body: "Test Body",
+      published: false,
+      author: {
+        connect: {
+          id
+        }
+      }
+    }
+  });
 });
 
 test("Should create a new user", async () => {
